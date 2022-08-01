73b67ca2054a39c139fc87a159eaec77f0f9f811
pzy <pengzhenyuan@gmail.com>
2022-08-01 23:44:38 +0800

    0801 Night

--- Personal-Book-Web/src/main.ts
+++ Personal-Book-Web/src/main.ts
@@ -10,6 +10,7 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
+import './icons'
 
 const app = createApp(App)
 
--- Personal-Book-Web/src/views/login/UserLogin.vue
+++ Personal-Book-Web/src/views/login/UserLogin.vue
@@ -1,12 +1,60 @@
 <template>
- <el-row :gutter="20">
-    <el-col :span="6"><div class="grid-content ep-bg-purple" /></el-col>
-    <el-col :span="6"><div class="grid-content ep-bg-purple" /></el-col>
-    <el-col :span="6"><div class="grid-content ep-bg-purple" /></el-col>
-    <el-col :span="6"><div class="grid-content ep-bg-purple" /></el-col>
-  </el-row>
+   <div>
+      <el-card class="login-form-layout">
+         <el-form autoComplete="on"  label-position="left">
+            <div style="text-align: center">
+               <svg-icon icon-class="login-mall" style="width: 56px;height: 56px;color: #409EFF"></svg-icon>
+            </div>
+            <h2 class="login-title color-main">Personal-Book</h2>
+            <el-form-item prop="username">
+               <el-input name="username" type="text" autoComplete="on"
+                  placeholder="请输入用户名">
+                  <!-- <span slot="prefix"> -->
+                     <svg-icon icon-class="user" class="color-main"></svg-icon>
+                  <!-- </span> -->
+               </el-input>
+            </el-form-item>
+            <el-form-item prop="password">
+               <el-input name="password" 
+                  autoComplete="on" placeholder="请输入密码">
+                  <!-- <span slot="prefix"> -->
+                     <svg-icon icon-class="password" class="color-main"></svg-icon>
+                  <!-- </span> -->
+                  <!-- <span slot="suffix"> -->
+                     <svg-icon icon-class="eye" class="color-main"></svg-icon>
+                  <!-- </span> -->
+               </el-input>
+            </el-form-item>
+            <el-form-item style="margin-bottom: 60px;text-align: center">
+               <el-button style="width: 50%;align-content: center;" type="primary">
+                  登录
+               </el-button>
+            </el-form-item>
+         </el-form>
+      </el-card>
+   </div>
 </template>
 
+
+<script lang="ts" setup>
+import { reactive } from 'vue'
+
+// do not use same name with ref
+const form = reactive({
+   name: '',
+   region: '',
+   date1: '',
+   date2: '',
+   delivery: false,
+   type: [],
+   resource: '',
+   desc: '',
+})
+
+const onSubmit = () => {
+   console.log('submit!')
+}
+</script>
    import qs from 'qs'
    export default {
       name: "Login",
@@ -73,23 +121,25 @@
    }
 
 <style scoped>
-   .el-col {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      height: 100%;
-      text-align: center;
-   }
-   .el-row {
-      height: 100%;
-      background-color: #fafafa;
-   }
-   .el-divider {
-      height: 200px;
-   }
-   .captchaImg {
-      float: left;
-      margin-left: 8px;
-      border-radius: 4px;
-   }
+ .login-form-layout {
+    position: absolute;
+    left: 0;
+    right: 0;
+    width: 360px;
+    margin: 140px auto;
+    border-top: 10px solid #409EFF;
+  }
+
+  .login-title {
+    text-align: center;
+  }
+
+  .login-center-layout {
+    background: #409EFF;
+    width: auto;
+    height: auto;
+    max-width: 100%;
+    max-height: 100%;
+    margin-top: 200px;
+  }
 </style>
